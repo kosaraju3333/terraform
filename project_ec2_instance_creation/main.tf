3d90c9632845aec9a3f91d4e26fe36eb2b68aabc
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-13 20:39:34 +0530

    added terraform file to create ec2 instance

---
+++ project_ec2_instance_creation/main.tf
@@ -0,0 +1,13 @@
+# Configure the AWS Provider
+provider "aws" {
+    region = "us-east-1"
+}
+
+# Crrating ec2 instance
+resource "aws_instance" "prometheus-VM" {
+    ami = "ami-031bc7545ba35f815"
+    instance_type = "t2.small"
+    subnet_id = "subnet-084a18eae9dbb2283"
+    key_name = "my_work"
+#    security_groups = "sg-01dd79cbdefbf5477"
+}
