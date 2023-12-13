f6e409a94b2fa50777d978b0e8529815955d8722
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-13 21:41:00 +0530

    added single region aws provider main.tf file

---
+++ providers/single-region-provider/main.tf
@@ -0,0 +1,17 @@
+# Configure the AWS Provider
+
+##### Single Region Provider Block #####
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+########################################
+
+# Crrating ec2 instance
+resource "aws_instance" "prometheus-VM" {
+    ami = "ami-031bc7545ba35f815"
+    instance_type = "t2.small"
+    subnet_id = "subnet-084a18eae9dbb2283"
+    key_name = "my_work"
+}
