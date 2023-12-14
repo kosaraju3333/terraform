b2476fdc17c4d630a0d75e0e56aa1dbebb871148
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-14 08:16:36 +0530

    Removed AMI_id Subnet_id and key_name values

--- providers/single-account-with-multiple-region-provider/main.tf
+++ providers/single-account-with-multiple-region-provider/main.tf
@@ -11,18 +11,18 @@ provider "aws" {
 ########################################
 
 resource "aws_instance" "ec2-us-east-1" {
-    ami = "ami-031bc7545ba35f815"
+    ami = "ami-ID"
     instance_type = "t2.small"
-    key_name = "my_work"
-    subnet_id = "subnet-084a18eae9dbb2283"
+    key_name = "pem.key"
+    subnet_id = "subnet-ID"
     provider = aws.us-east-1
 
 }
 
 resource "aws_instance" "ec2-ap-south-1" {
-    ami = "ami-0a2bece398df13b9d"
+    ami = "ami-ID"
     instance_type = "t2.small"
-    key_name = "ap-south-1"
+    key_name = "pem.key"
     provider = aws.ap-south-1
 
 }
