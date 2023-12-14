0f3deccac2bd0e94789645d7ed76e928c3a200b8
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-14 08:18:50 +0530

    Removed ami_id, subnet_id and key_name values

--- providers/single-region-provider/main.tf
+++ providers/single-region-provider/main.tf
@@ -1,17 +1,15 @@
 # Configure the AWS Provider
 
 ##### Single Region Provider Block #####
-
 provider "aws" {
     region = "us-east-1"
 }
-
 ########################################
 
 # Crrating ec2 instance
 resource "aws_instance" "prometheus-VM" {
-    ami = "ami-031bc7545ba35f815"
+    ami = "ami-ID"
     instance_type = "t2.small"
-    subnet_id = "subnet-084a18eae9dbb2283"
-    key_name = "my_work"
+    subnet_id = "subnet-ID"
+    key_name = "pem.key"
 }
