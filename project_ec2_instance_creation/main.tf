897ced7318a7d613f8ee6c4988e78af10954324c
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-14 08:13:52 +0530

    Removed Subnet_id and key_name values

--- project_ec2_instance_creation/main.tf
+++ project_ec2_instance_creation/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 # Crrating ec2 instance
 resource "aws_instance" "prometheus-VM" {
-    ami = "ami-031bc7545ba35f815"
+    ami = "ami-ID"
     instance_type = "t2.small"
-    subnet_id = "subnet-084a18eae9dbb2283"
-    key_name = "my_work"
+    subnet_id = "subnet-ID"
+    key_name = "pem.key"
 }
