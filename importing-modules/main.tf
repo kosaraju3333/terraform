8c43daed798fe7845198487bca45814b2909f894
kosaraju3333 <kosaraju3333@gmail.com>
2023-12-15 07:23:43 +0530

    Importing terraform modules in to main.tf file

---
+++ importing-modules/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+#  source = "/Users/ramakrishnakosaraju/spontansolutions/terraform/modules/ec2_instance_creation"
+  source = "../modules/ec2_instance_creation"
+  ami_id_value = "ami-031bc7545ba35f815"
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-084a18eae9dbb2283"
+  key_name_value = "my_work"
+}
